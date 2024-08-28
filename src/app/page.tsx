52a0ce7a74e66186f7afd2956469ec9e2aeeb991
ViniciusTei <viniciustprates@gmail.com>
2024-08-28 16:10:44 -0300

    fix home styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex-1 flex-col items-center justify-between bg-undraw-bloom bg-contain bg-no-repeat bg-center">
-      <h1 className="text-4xl font-semibold text-green-600">Welcome</h1>
-      <p className="text-green-600">We’re glad that you are here!</p>
-      <div className="items-center justify-center mt-auto">
+    <main className="h-full flex flex-col items-center justify-between bg-undraw-bloom bg-contain bg-no-repeat bg-center md:bg-right md:bg-[length:600px_400px]">
+      <h1 className="text-6xl font-semibold text-green-600 mt-24">Welcome</h1>
+      <p className="text-green-600 text-lg">We’re glad that you are here!</p>
+      <div className="items-center justify-center mt-auto mb-24">
         <Button className="mt-auto">
           <Link href="/login" scroll={false}>
             Let’s get started
