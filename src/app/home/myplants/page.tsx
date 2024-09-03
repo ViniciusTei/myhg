e55ba8c918d6eb95d88c84f37aec52c3144bc84b
ViniciusTei <viniciustprates@gmail.com>
2024-09-03 08:02:37 -0300

    fix plant list overflow

--- src/app/home/myplants/page.tsx
+++ src/app/home/myplants/page.tsx
@@ -13,8 +13,8 @@ export default function Page() {
     redirect('/login')
   }
   return (
-    <main className="h-full w-full">
-      <header className="bg-white w-full max-w-screen-md mx-auto p-4">
+    <div className="w-full overflow-y-auto pb-14">
+      <header className="bg-white w-full max-w-screen-md mx-auto p-4 sticky top-0 z-50">
         <h1 className="font-bold mb-4">My Plants</h1>
         <div className="relative">
           <Input type="text" placeholder="Search plants" className="w-full rounded-full border border-gray-300 py-2 pl-4 pr-10 focus:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-200" />
@@ -22,6 +22,6 @@ export default function Page() {
         </div>
       </header>
       <PlantsList />
-    </main>
+    </div>
   )
 }
