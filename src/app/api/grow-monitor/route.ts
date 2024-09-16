52abd5a05102a54aca863e35151d3a24eb257a7f
ViniciusTei <viniciustprates@gmail.com>
2024-09-16 09:14:20 -0300

    fix handle errors from async calls

--- src/app/api/grow-monitor/route.ts
+++ src/app/api/grow-monitor/route.ts
@@ -6,75 +6,89 @@ const queries = {
 }
 
 export async function GET(request: Request, response: Response) {
-  const client = turso()
-  const { rows: currentInfoRows } = await client.execute('SELECT * from grow_monitor order by id DESC limit 1')
+  try {
 
-  if (currentInfoRows.length !== 1) {
+    const client = turso()
+    const { rows: currentInfoRows } = await client.execute('SELECT * from grow_monitor order by id DESC limit 1')
+
+    if (currentInfoRows.length !== 1) {
+      return NextResponse.json(
+        {
+          message: "Error! Cannot get current info."
+        },
+        {
+          status: 500,
+        }
+      )
+    }
+
+    const { rows: allInfoRows } = await client.execute(queries.lastDay)
+
+    if (allInfoRows.length === 0) {
+      return NextResponse.json(
+        {
+          message: "Error! Cannot get all info."
+        },
+        {
+          status: 500,
+        }
+      )
+    }
+
+    const selectedRows = [];
+
+    const groupedByHour = allInfoRows.reduce((acc, row) => {
+      const hourGroup = row.hour_group as unknown as any;
+      if (!hourGroup) return acc
+      if (!acc[hourGroup]) {
+        acc[hourGroup] = [];
+      }
+      acc[hourGroup].push(row);
+      return acc;
+    }, {} as any);
+
+    for (const hour in groupedByHour) {
+      const records = groupedByHour[hour];
+
+      // Se existirem 5 ou menos registros, mantemos todos
+      if (records.length <= 5) {
+        selectedRows.push(...records);
+      } else {
+        // Pegar o primeiro, três do meio, e o último
+        const first = records[0];
+        const last = records[records.length - 1];
+
+        // Calcular o meio
+        const midStart = Math.floor((records.length - 1) / 2) - 1;
+        const middleRecords = records.slice(midStart, midStart + 3);
+
+        selectedRows.push(first, ...middleRecords, last);
+      }
+    }
+
     return NextResponse.json(
       {
-        message: "Error! Cannot get current info."
+        message: "Sucesso!",
+        data: {
+          current: currentInfoRows[0],
+          all: selectedRows
+        }
       },
       {
-        status: 500,
+        status: 200,
+      }
+    )
+  } catch (err) {
+    return NextResponse.json(
+      {
+        message: "Error!",
+        error: err
+      },
+      {
+        status: (err as any).status ?? 500,
       }
     )
   }
-
-  const { rows: allInfoRows, columns } = await client.execute(queries.lastDay)
-
-  if (allInfoRows.length === 0) {
-    return NextResponse.json(
-      {
-        message: "Error! Cannot get all info."
-      },
-      {
-        status: 500,
-      }
-    )
-  }
-
-  const selectedRows = [];
-
-  const groupedByHour = allInfoRows.reduce((acc, row) => {
-    const hourGroup = row.hour_group as unknown as any;
-    if (!hourGroup) return acc
-    if (!acc[hourGroup]) {
-      acc[hourGroup] = [];
-    }
-    acc[hourGroup].push(row);
-    return acc;
-  }, {} as any);
-
-  for (const hour in groupedByHour) {
-    const records = groupedByHour[hour];
-
-    // Se existirem 5 ou menos registros, mantemos todos
-    if (records.length <= 5) {
-      selectedRows.push(...records);
-    } else {
-      // Pegar o primeiro, três do meio, e o último
-      const first = records[0];
-      const last = records[records.length - 1];
-
-      // Calcular o meio
-      const midStart = Math.floor((records.length - 1) / 2) - 1;
-      const middleRecords = records.slice(midStart, midStart + 3);
-
-      selectedRows.push(first, ...middleRecords, last);
-    }
-  }
-  return NextResponse.json(
-    {
-      message: "Sucesso!",
-      data: {
-        current: currentInfoRows[0],
-        all: selectedRows
-      }
-    },
-    {
-      status: 200,
-    }
-  )
 }
 
 export async function POST(request: Request, response: Response) {
