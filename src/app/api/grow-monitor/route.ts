759e2f3ce24d95afd7d7212deb2bfc01e9d8e4b0
ViniciusTei <viniciustprates@gmail.com>
2024-09-16 06:55:09 -0300

    fix grow monitor query to fetch last 24 hours data only

--- src/app/api/grow-monitor/route.ts
+++ src/app/api/grow-monitor/route.ts
@@ -1,6 +1,10 @@
 import { turso } from "@/lib/turso"
 import { NextResponse } from "next/server"
 
+const queries = {
+  lastDay: 'SELECT *, strftime(\'%Y-%m-%d %H:00\', time_iso) as hour_group FROM grow_monitor WHERE datetime(time_iso) >= datetime(\'now\', \'-24 hours\') ORDER BY time_iso;'
+}
+
 export async function GET(request: Request, response: Response) {
   const client = turso()
   const { rows: currentInfoRows } = await client.execute('SELECT * from grow_monitor order by id DESC limit 1')
@@ -16,7 +20,7 @@ export async function GET(request: Request, response: Response) {
     )
   }
 
-  const { rows: allInfoRows } = await client.execute('SELECT * from grow_monitor')
+  const { rows: allInfoRows, columns } = await client.execute(queries.lastDay)
 
   if (allInfoRows.length === 0) {
     return NextResponse.json(
@@ -29,12 +33,42 @@ export async function GET(request: Request, response: Response) {
     )
   }
 
+  const selectedRows = [];
+
+  const groupedByHour = allInfoRows.reduce((acc, row) => {
+    const hourGroup = row.hour_group as unknown as any;
+    if (!hourGroup) return acc
+    if (!acc[hourGroup]) {
+      acc[hourGroup] = [];
+    }
+    acc[hourGroup].push(row);
+    return acc;
+  }, {} as any);
+
+  for (const hour in groupedByHour) {
+    const records = groupedByHour[hour];
+
+    // Se existirem 5 ou menos registros, mantemos todos
+    if (records.length <= 5) {
+      selectedRows.push(...records);
+    } else {
+      // Pegar o primeiro, três do meio, e o último
+      const first = records[0];
+      const last = records[records.length - 1];
+
+      // Calcular o meio
+      const midStart = Math.floor((records.length - 1) / 2) - 1;
+      const middleRecords = records.slice(midStart, midStart + 3);
+
+      selectedRows.push(first, ...middleRecords, last);
+    }
+  }
   return NextResponse.json(
     {
       message: "Sucesso!",
       data: {
         current: currentInfoRows[0],
-        all: allInfoRows
+        all: selectedRows
       }
     },
     {
