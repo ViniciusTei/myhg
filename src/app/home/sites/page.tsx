cb40ce590cef9d948a95ee39417d8da7b1c578c0
ViniciusTei <viniciustprates@gmail.com>
2024-10-16 07:22:40 -0300

    fix space

--- src/app/home/sites/page.tsx
+++ src/app/home/sites/page.tsx
@@ -28,7 +28,11 @@ export default async function SitesPage() {
       <header className="bg-white w-full max-w-screen-md mx-auto p-4 sticky top-0 z-50">
         <h1 className="font-bold mb-4">Sites</h1>
         <div className="relative">
-          <Input type="text" placeholder="Search sites" className="w-full rounded-full border border-gray-300 py-2 pl-4 pr-10 focus:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-200" />
+          <Input
+            type="text"
+            placeholder="Search sites"
+            className="w-full rounded-full border border-gray-300 py-2 pl-4 pr-10 focus:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-200"
+          />
           <Search className="absolute right-3 top-1/2 h-5 w-5 -translate-y-1/2 text-gray-400" />
         </div>
 
